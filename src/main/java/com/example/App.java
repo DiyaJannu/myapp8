cd7206a0bdd599c9d2b618ddab0e4af67e4faea7
DiyaJannu <reeyajannu9@gamil.com>
2025-06-08 14:02:18 +0000

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,17 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int a=2;
+        int b=3;
+        int c=a+b;
+        System.out.println("2+3="+c);
+        System.out.println( "Hello World!" );
+    }
+}
